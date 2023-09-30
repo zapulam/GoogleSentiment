e68537be4d78e5c0d739c8cc80d25c024a1880e5
Zack's PC <zapulliam22@gmail.com>
2023-09-30 00:54:19 -0500

    initial flask app

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from transformers import pipeline
+from GoogleNews import GoogleNews
+from flask import Flask, render_template, request
 
 # Create a Flask web app instance
 app = Flask(__name__)
 
 # Define a route for the root URL ("/")
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    user_input = ""
+    
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+    
+    return render_template("index.html", user_input=user_input)
 
 # Run the app if this script is executed
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
