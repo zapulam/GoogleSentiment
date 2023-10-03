c4596309ea0b9e8a4205cc42d963984eded9bbb1
Pulliam <zachary.pulliam@capgemini.com>
2023-10-03 01:05:04 -0500

    update app

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+import pandas as pd
+
 from transformers import pipeline
 from GoogleNews import GoogleNews
 from flask import Flask, render_template, request
 
+from functions import *
+
 # Create a Flask web app instance
 app = Flask(__name__)
+
+# Initialize GoogleNews instance
+googlenews = GoogleNews()
+googlenews.enableException(True)
+
+# Initialize text models
+sentimentizer = pipeline("sentiment-analysis", model ='distilbert-base-uncased-finetuned-sst-2-english')
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # Define a route for the root URL ("/")
 @app.route("/", methods=["GET", "POST"])
@@ -12,9 +24,29 @@ def index():
     
     if request.method == "POST":
         user_input = request.form["user_input"]
+
+    headlines = get_google_news_headlines(googlenews, user_input)
+
+    if headlines:
+        top_n = "\nTop {} headlines pulled...".format(len(headlines))
+
+        sentiments, avg_sentiment, maxSentiment, maxSentimentIdx, minSentiment, minSentimentIdx = report_sentiment(sentimentizer, headlines)
+
+        high_text = '\nHighest rated sentiment of "{}" given to the following headline: \n"{}"'.format(maxSentiment, headlines[maxSentimentIdx])
+        low_text = '\nLowest rated sentiment of "{}" given to the following headline: \n"{}"'.format(minSentiment, headlines[minSentimentIdx])
+
+        df = pd.DataFrame({'Sentiments': sentiments, 'Headlines': headlines})
+        df.sort_values(by='Sentiments', inplace=True)
+
+        summary = summarize_headlines(summarizer, df['Headlines'])
+
+    else:
+        top_n = "No headlines found."
+        high_text, low_text = "", ""
+        summary = ""
     
     return render_template("index.html", user_input=user_input)
 
 # Run the app if this script is executed
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ from GoogleNews import GoogleNews
 from transformers import pipeline
 from transformers import logging
 
+
 # Function to fetch headlines based on a user-specified prompt
 def get_google_news_headlines(googlenews, prompt):
     try:
@@ -20,19 +21,40 @@ def get_google_news_headlines(googlenews, prompt):
 
 
 # Returns avergae sentiment score of all headlines
-def report_sentiment(headlines):
+def report_sentiment(sentimentizer, headlines):
     try:
-        sentiment_pipeline = pipeline("sentiment-analysis", model ='distilbert-base-uncased-finetuned-sst-2-english')
-        sentiments = sentiment_pipeline(headlines)
+        sentiments = sentimentizer(headlines)
 
         for i, sentiment in enumerate(sentiments):
             if sentiment['label'] == 'NEGATIVE':
                 sentiments[i]['score'] = sentiments[i]['score'] * -1
 
+        avg_sentiment = sum(sentiment['score'] for sentiment in sentiments) / len(sentiments)
+
+        maxSentiment = round(max(sentiments, key=lambda x:x['score'])['score'], 5)
+        maxSentimentIdx = max(range(len(sentiments)), key=lambda i: sentiments[i].get('score', 0))
+
+        minSentiment = round(min(sentiments, key=lambda x:x['score'])['score'], 5)
+        minSentimentIdx = min(range(len(sentiments)), key=lambda i: sentiments[i].get('score', 0))
+
+        return sentiments, avg_sentiment, maxSentiment, maxSentimentIdx, minSentiment, minSentimentIdx
+
     except Exception as e:
         return str(e)
+
+
+# Summarize headlines
+def summarize_headlines(summarizer, headlines):
+    try:
+        headlines = '. '.join(headlines.tolist()[:512])
+        tokens = headlines.split()
+        first_N_tokens = ' '.join(tokens[:512])
+
+        summary = summarizer(first_N_tokens, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+        return summary
     
-    return sentiments, sum(sentiment['score'] for sentiment in sentiments) / len(sentiments)
+    except Exception as e:
+        return str(e)
 
 
 # Main function
@@ -72,7 +94,3 @@ def main():
 
     else:
         print("No headlines found.")
-
-
-if __name__ == "__main__":
-    main()
